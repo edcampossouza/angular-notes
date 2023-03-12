52b0bf142d97c5b3db7fa903b80dac22b2c67686
Eduardo Souza <edcampossouza@gmail.com>
2023-03-12 18:11:49 -0300

    feat: animate notes

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,3 +1,11 @@
+import {
+  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
@@ -6,6 +14,90 @@ import { NotesService } from 'src/app/shared/notes.service';
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
   styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      //ENTRY ANIMATION
+      transition('void => *', [
+        // SET INITIAL
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          marginBottom: 0,
+          paddingTop: 0,
+          paddingRight: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+        }),
+        animate(
+          '50ms',
+          style({
+            height: '*',
+            transform: 'scale(0.85)',
+            marginBottom: '*',
+            paddingTop: '*',
+            paddingRight: '*',
+            paddingBottom: '*',
+            paddingLeft: '*',
+          })
+        ),
+        animate(68),
+      ]),
+      transition('* => void', [
+        // first scale up
+        animate(
+          '50ms',
+          style({
+            transform: 'scale(1.05)',
+          })
+        ),
+        // then scale down while beginning to fade out
+        animate(
+          '50ms',
+          style({
+            transform: 'scale(1)',
+            opacity: 0.7,
+          })
+        ),
+        //scale down
+        animate(
+          '120ms ease-out',
+          style({
+            opacity: 0,
+            transform: 'scale(0.68)',
+          })
+        ),
+        // then animate the spacing
+        animate(
+          '150ms ease-out',
+          style({
+            opacity: 0,
+            height: 0,
+            paddingTop: 0,
+            paddingRight: 0,
+            paddingBottom: 0,
+            paddingLeft: 0,
+            marginBottom: 0,
+          })
+        ),
+      ]),
+    ]),
+    trigger('listAnim', [
+      transition('* => *', [
+        query(
+          ':enter',
+          [
+            style({
+              opacity: 0,
+              height: 0,
+            }),
+            stagger('100ms', [animate('0.2s ease')]),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
+  ],
 })
 export class NotesListComponent {
   notes: Note[] = new Array<Note>();
