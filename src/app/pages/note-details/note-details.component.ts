c5dbd43bf2d6d8e3361fad25bd208590e76d57ec
Eduardo Souza <edcampossouza@gmail.com>
2023-03-12 10:39:40 -0300

    feat: edit note

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 
@@ -11,16 +11,41 @@ import { NotesService } from 'src/app/shared/notes.service';
 })
 export class NoteDetailsComponent {
   note!: Note;
+  noteId!: number;
+  new!: boolean;
 
-  constructor(private noteService: NotesService, private router: Router) {}
+  constructor(
+    private noteService: NotesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.note = new Note();
+    // find out if creating or editing
+    this.route.params.subscribe((params: Params) => {
+      this.note = new Note();
+      if (params['id']) {
+        this.note = this.noteService.get(params['id']);
+        this.noteId = params['id'];
+        this.new = false;
+      } else {
+        this.new = true;
+      }
+    });
   }
 
   onSubmit(form: NgForm) {
-    // save the note
-    this.noteService.add(form.value);
-    this.router.navigateByUrl("/");
+    if (this.new) {
+      // save the note
+      this.noteService.add(form.value);
+    } else {
+      // update the note
+      this.noteService.update(this.noteId, form.value.title, form.value.body);
+    }
+    this.router.navigateByUrl('/');
+  }
+
+  cancel() {
+    this.router.navigateByUrl('/');
   }
 }
